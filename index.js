6cc946317b2a9ceb4b1780a84b675e91013350ac
santi <santiago.ezequiel.munoz@gmail.com>
2024-05-25 14:31:56 -0300

    entrega1

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 let nombre = prompt("Bienvenido al supermercado Fresh, por favor ingresar su nombre:");
-let edad =  prompt("Hola " + nombre + ", a continuacion deberas ingresar tu edad:");
+let edad =  Number(prompt("Hola " + nombre + ", a continuacion deberas ingresar tu edad:"));
     let option;
     let total = 0;
+    function sumadecompra(x){
+        total = total + x;
+    }
+    function totaldecompra(producto){
+        alert("Has añadido " + producto + "\n La compra por ahora es de $" + total);
+    }
 if (edad < 18){
     while(option != 0) {
         option = prompt("Estos son los productos disponibles, cual/es desea llevar: \n 1. Gaseosa Coca-Cola 2.25lt $2000  \n 2. Agua Saborizada Aquarius Pera 2.25lt $1400 \n 3. Gaseosa Sprite Lima-Limon 2.25lt $2600  \n 4. Bebida Energizante Monster Energy 473ml $1900 \n 5. Finalizar compra \n 0. salir");
         if(option == 1){
-            total= total + 2000;
-            alert("Has añadido una Coca-cola.  \n La compra por ahora es de $" + total);
+            sumadecompra(2000);
+            totaldecompra("una Coca-Cola.");
         } else if(option == 2){
-            total= total + 1400;
-            alert("Has añadido una Aquarius de Pera. \n La compra por ahora es de $" + total);
+            sumadecompra(1400);
+            totaldecompra("una Aquarius de Pera.");
         } else if(option == 3){
-            total= total + 2600;
-            alert("Has añadido una Sprite.  \n La compra por ahora es de $" + total);
+            sumadecompra(2600);
+            totaldecompra("una Sprite.");
         } else if(option == 4){
-            total= total + 1900;
-            alert("Has añadido una Monster.  \n La compra por ahora es de $" + total);
+            sumadecompra(1900);
+            totaldecompra("una Monster.");
         } else if(option == 5){
             if(total != 0){
             alert("Has finalizado la compra. El total a pagar es de $" + total + ". \n Muchas gracias por la compra, vuelva pronto.");
@@ -27,34 +33,32 @@ if (edad < 18){
         }else if(option == 0){
                 if(total != 0){
                 alert("Se cancelo tu compra ya que olvidaste de finalizarla.");
-                
             }else if(total == 0){
             alert("Gracias por la visita");
             } 
         } 
     }
     } else if(edad > 17){
-    
     while(option != 0){
         option = prompt("Estos son los productos disponibles, cual/es desea llevar: \n 1. Gaseosa Coca-Cola 2.25lt $2000  \n 2. Agua Saborizada Aquarius Pera 2.25lt $1400 \n 3. Gaseosa Sprite Lima-Limon 2.25lt $2600  \n 4. Bebida Energizante Monster Energy 473ml $1900 \n 5. Fernet Branca 750ml $9200 \n 6. Vodka Smirnoff Raspberry 700ml $5900 \n 7.Finalizar compra \n 0. salir");
         if(option == 1){
-            total= total + 2000;
-            alert("Has añadido una Coca-cola.  \n La compra por ahora es de $" + total);
+            sumadecompra(2000);
+            totaldecompra("una Coca-Cola.");
         } else if(option == 2){
-            total= total + 1400;
-            alert("Has añadido una Aquarius de Pera. \n La compra por ahora es de $" + total);
+            sumadecompra(1400);
+            totaldecompra("una Aquarius de Pera.");
         } else if(option == 3){
-            total= total + 2600;
-            alert("Has añadido una Sprite.  \n La compra por ahora es de $" + total);
+            sumadecompra(2600);
+            totaldecompra("una Sprite.");
         } else if(option == 4){
-            total= total + 1900;
-            alert("Has añadido una Monster.  \n La compra por ahora es de $" + total);
+            sumadecompra(1900);
+            totaldecompra("una Monster.");
         } else if(option ==5){
-            total= total + 9200; 
-            alert("Has añadido un fernet. \n La compra por ahorass es de $" + total);
+            sumadecompra(9200); 
+            totaldecompra("un Fernet.");
         } else if(option == 6){
-            total= total + 5900;
-            alert("Has añadido un Smirnoff. \n La compra por ahora es de $" + total);
+            sumadecompra(5900);
+            totaldecompra("un Smirnoff.");
         } else if(option == 7){
             if(total == 0){
                 alert("No has añadido nada al carrito");
