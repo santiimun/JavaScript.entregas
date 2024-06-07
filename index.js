d9d84246b500a6a6c9e6f6b5cfaf937139392a13
santi <santiago.ezequiel.munoz@gmail.com>
2024-06-06 21:40:39 -0300

    2da entrega js

--- index.js
+++ index.js
@@ -1,81 +1,167 @@
-let nombre = prompt("Bienvenido al supermercado Fresh, por favor ingresar su nombre:");
-let edad =  Number(prompt("Hola " + nombre + ", a continuacion deberas ingresar tu edad:"));
-    let option;
-    let total = 0;
-    function sumadecompra(x){
-        total = total + x;
+//////////////CREACION DE PRODUCTOS////////////////////
+
+
+
+let todosLosProductos = []
+
+function agregarProducto(nombre, precio, prenda, genero) {
+    let nuevoProducto = {
+        nombre: nombre,
+        precio: precio,
+        prenda: prenda,
+        genero: genero,
     }
-    function totaldecompra(producto){
-        alert("Has añadido " + producto + "\n La compra por ahora es de $" + total);
-    }
-if (edad < 18){
-    while(option != 0) {
-        option = prompt("Estos son los productos disponibles, cual/es desea llevar: \n 1. Gaseosa Coca-Cola 2.25lt $2000  \n 2. Agua Saborizada Aquarius Pera 2.25lt $1400 \n 3. Gaseosa Sprite Lima-Limon 2.25lt $2600  \n 4. Bebida Energizante Monster Energy 473ml $1900 \n 5. Finalizar compra \n 0. salir");
-        if(option == 1){
-            sumadecompra(2000);
-            totaldecompra("una Coca-Cola.");
-        } else if(option == 2){
-            sumadecompra(1400);
-            totaldecompra("una Aquarius de Pera.");
-        } else if(option == 3){
-            sumadecompra(2600);
-            totaldecompra("una Sprite.");
-        } else if(option == 4){
-            sumadecompra(1900);
-            totaldecompra("una Monster.");
-        } else if(option == 5){
-            if(total != 0){
-            alert("Has finalizado la compra. El total a pagar es de $" + total + ". \n Muchas gracias por la compra, vuelva pronto.");
-            break;
-            } else if(total == 0){
-                alert("No has añadido nada al carrito");
-            }
-        }else if(option == 0){
-                if(total != 0){
-                alert("Se cancelo tu compra ya que olvidaste de finalizarla.");
-            }else if(total == 0){
-            alert("Gracias por la visita");
-            } 
-        } 
-    }
-    } else if(edad > 17){
-    while(option != 0){
-        option = prompt("Estos son los productos disponibles, cual/es desea llevar: \n 1. Gaseosa Coca-Cola 2.25lt $2000  \n 2. Agua Saborizada Aquarius Pera 2.25lt $1400 \n 3. Gaseosa Sprite Lima-Limon 2.25lt $2600  \n 4. Bebida Energizante Monster Energy 473ml $1900 \n 5. Fernet Branca 750ml $9200 \n 6. Vodka Smirnoff Raspberry 700ml $5900 \n 7.Finalizar compra \n 0. salir");
-        if(option == 1){
-            sumadecompra(2000);
-            totaldecompra("una Coca-Cola.");
-        } else if(option == 2){
-            sumadecompra(1400);
-            totaldecompra("una Aquarius de Pera.");
-        } else if(option == 3){
-            sumadecompra(2600);
-            totaldecompra("una Sprite.");
-        } else if(option == 4){
-            sumadecompra(1900);
-            totaldecompra("una Monster.");
-        } else if(option ==5){
-            sumadecompra(9200); 
-            totaldecompra("un Fernet.");
-        } else if(option == 6){
-            sumadecompra(5900);
-            totaldecompra("un Smirnoff.");
-        } else if(option == 7){
-            if(total == 0){
-                alert("No has añadido nada al carrito");
-                continue;
-            }
-            alert("Has finalizado la compra. El total a pagar es de $" + total + ". \n Muchas gracias por la compra, vuelva pronto.");
-            break;
-        } else if(option == 0){
-                if(total != 0){
-                alert("Se cancelo tu compra ya que olvidaste de finalizarla.");
-            }else if(total == 0){
-            alert("Gracias por la visita");
-            }
+    todosLosProductos.push(nuevoProducto)
+}
+
+agregarProducto("Zapatillas Adi2000 de hombre", 160000, "calzado", "hombre");
+agregarProducto("Zapatillas Jordan Air 4 Retro de hombre", 320000, "calzado", "hombre");
+agregarProducto("Zapatillas Puma Suede XL de Hombre", 170000, "calzado", "hombre");
+agregarProducto("Pantalon Adidas Originals Essentials de hombre", 75000, "indumentaria", "hombre");
+agregarProducto("Remera Jordan Air de hombre", 60000, "indumentaria", "hombre");
+agregarProducto("Campera Nike Tech Fleece de hombre", 190000, "indumentaria", "hombre");
+agregarProducto("Zapatillas Jordan Air 1 de mujer", 220000, "calzado", "mujer");
+agregarProducto("Zapatillas Nike AF1 Shadow de mujer", 260000, "calzado", "mujer");
+agregarProducto("Zapatillas Adidas Originals Campus 00s de mujer", 170000, "calzado", "mujer");
+agregarProducto("Chaleco Nike Classic de mujer", 140000, "indumentaria", "mujer");
+agregarProducto("Short Jordan Classic de mujer", 85000, "indumentaria", "mujer");
+agregarProducto("Buzo Champion de mujer", 52000, "indumentaria", "mujer");
+agregarProducto("Zapatillas Adidas Originals Superstar de niño", 84000, "calzado", "niños");
+agregarProducto("Zapatillas Puma Suede Classic XXI de niños", 60000, "calzado", "niños");
+agregarProducto("Zapatillas Nike Air Force 1 06", 55000, "calzado", "niños");
+agregarProducto("Pantalon Jordan Essentials de niño", 58000, "indumentaria", "niños");
+agregarProducto("Remera Adidas Originals Trefoil de niños", 45000, "indumentaria", "niños");
+agregarProducto("Remera Nike Crop de niño", 35000, "indumentaria", "niños");
+
+
+
+
+
+
+////////////DIVISION DE LOS PRODUCTOS/////////////
+
+//////CALZADO/////////
+
+let calzado = todosLosProductos.filter((el) => el.prenda == "calzado");
+let nombresDeCalzado = calzado.map((producto) => { return producto.nombre });
+let precioCalzado = calzado.map((producto) => { return producto.precio });
+let listaCalzado = "";
+for (let i = 0; i < nombresDeCalzado.length; i++) {
+    listaCalzado += (i + 1) + ". " + nombresDeCalzado[i] + " $" + precioCalzado[i] + "\n";
+}
+
+/////////////INDUMENTARIA///////////////
+
+
+let indumentaria = todosLosProductos.filter((el) => el.prenda == "indumentaria");
+let nombreIndumentaria = indumentaria.map((producto) => { return producto.nombre });
+let precioIndumentaria = indumentaria.map((producto) => { return producto.precio });
+let listaIndumentaria = "";
+for (let i = 0; i < nombreIndumentaria.length; i++) {
+    listaIndumentaria += (i + 1) + ". " + nombreIndumentaria[i] + " $" + precioIndumentaria[i] + "\n";
+}
+
+
+
+
+
+////////////CICLOS/////////////
+
+
+function carritoCalzado(a) {
+    let productoAniadido = "-" + nombresDeCalzado[a] + " $" + precioCalzado[a];
+    carritoDeCompras.push(productoAniadido);
+    total = total + precioCalzado[a]
+    alert("Añadiste: " + nombresDeCalzado[a] + "  $"+ precioCalzado[a])
+}
+function carritoIndumentaria(a) {
+    let productoAniadido = "-" + nombreIndumentaria[a] + " $" + precioIndumentaria[a];
+    carritoDeCompras.push(productoAniadido);
+    total = total + precioIndumentaria[a]
+    alert("Añadiste: " + nombreIndumentaria[a] + "  $"+ precioIndumentaria[a]);
+}
+
+
+
+
+let option;
+let total = 0;
+let carritoDeCompras = []
+
+
+
+while (option !== 0) {
+    option = Number(prompt("Bienvenido a Pakani. Ingrese la opción sobre la cual está interesado: \n 1. Calzado \n 2. Indumentaria \n 3. Finalizar compra \n 0. Salir"));
+    if (option === 1) {
+        let valor = Number(prompt("Estos son los productos disponibles: \n" + listaCalzado + "0. Salir"));
+        switch (valor) {
+            case 1:
+                carritoCalzado(0);
+                break;
+            case 2:
+                carritoCalzado(1);
+                break;
+            case 3:
+                carritoCalzado(2);
+                break;
+            case 4:
+                carritoCalzado(3);
+                break;
+            case 5:
+                carritoCalzado(4);
+                break;
+            case 6:
+                carritoCalzado(5);
+                break;
+            case 7:
+                carritoCalzado(6);
+                break;
+            case 8:
+                carritoCalzado(7);
+                break;
+            case 9:
+                carritoCalzado(8);
+                break;
         }
+    } else if (option === 2) {
+        let valorB = Number(prompt("Estos son los productos disponibles: \n" + listaIndumentaria + "0. Salir"));
+        switch (valorB) {
+            case 1:
+                carritoIndumentaria(0);
+                break;
+            case 2:
+                carritoIndumentaria(1);
+                break;
+            case 3:
+                carritoIndumentaria(2);
+                break;
+            case 4:
+                carritoIndumentaria(3);
+                break;
+            case 5:
+                carritoIndumentaria(4);
+                break;
+            case 6:
+                carritoIndumentaria(5);
+                break;
+            case 7:
+                carritoIndumentaria(6);
+                break;
+            case 8:
+                carritoIndumentaria(7);
+                break;
+            case 9:
+                carritoIndumentaria(8);
+                break;
+        }
+    } else if (option === 3) {
+        if (total === 0) {
+            alert("No has ingresado nada al carrito");
+        } else if (total !== 0) {
+            alert(carritoDeCompras.join("\n")+ "\n \n El total a pagar es $" + total + " \n Muchas Gracias por la compra. Hasta luego");
+            break;
+        }
+    } else if (option === 0) {
+        alert("Muchas Gracias por visitarnos. Nos vemos pronto");
     }
-} 
-
-
-
-
+}
